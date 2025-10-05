8cd522962468a2aa6d75ef8efd16c3f1d6047a4e
Anton -- Lev1nzz <xlev1n.xxxx@gmail.com>
2025-10-05 12:10:22 +0300

    add docstr in class

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,9 +1,7 @@
 # Объявить класс.
 class Board:
-    '''
-    Инициализировать игровое поле - список списков с пробелами.
-    Пробелы - это пустые клетки.
-    '''
+    """Класс, который описывает игровое поле."""
+    
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
         
@@ -16,4 +14,10 @@ class Board:
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+    
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        ) 
             
